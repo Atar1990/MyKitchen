1f8de2b91f1bcaf1320626eb89522eae97ba652e
Atar1990 <atarneofeld@gmail.com>
2022-12-09 15:31:48 +0200

    fix

--- my-app/src/Functional Comps/Recipies.jsx
+++ my-app/src/Functional Comps/Recipies.jsx
@@ -6,7 +6,7 @@ export default function Recipies(props) {
     return <div className="recipes">
         {props.list.map(item => {
             return (
-                <Recipe key={item.id} item={item} handleClick={() => props.handleClick(item.id)}></Recipe>
+                <Recipe key={item.id} item={item} handleClick={() => props.onClick(item.id)}></Recipe>
             )
         })}
     </div>
