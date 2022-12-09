734d3016ebf2bcc7a69400932db21ca735cc8891
Atar1990 <atarneofeld@gmail.com>
2022-12-09 16:20:31 +0200

    functional component only

--- my-app/src/Classs Componenets/CCMyKitchen.jsx
+++ my-app/src/Classs Componenets/CCMyKitchen.jsx
@@ -1,42 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Recipies from "../Functional Comps/Recipies";
 
-export default class MyKitchen extends React.Component {
+export default function MyKitchen() {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            list: [
-                {
-                    id: 1,
-                    name: 'Burekas',
-                    img: './picturs/burekasChicken.png',
-                    calories: 876,
-                    isReady: false
-                },
-                {
-                    id: 2,
-                    name: 'Pasta',
-                    img: './picturs/pastaSussage.png',
-                    calories: 444,
-                    isReady: false
-                },
-                {
-                    id: 3,
-                    name: 'Carrot',
-                    img: './picturs/carrot.jpeg',
-                    calories: 100,
-                    isReady: false
-                }
-            ]
-        };
-    }
-    // runnig over the list 
-    // copy item
-    // change isReady if id match
-    // save new item
-    setReadyOnItem(id) {
-        this.setState(this.state.list.map(item => {
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            name: 'Burekas',
+            img: './picturs/burekasChicken.png',
+            calories: 876,
+            isReady: false
+        },
+        {
+            id: 2,
+            name: 'Pasta',
+            img: './picturs/pastaSussage.png',
+            calories: 444,
+            isReady: false
+        },
+        {
+            id: 3,
+            name: 'Carrot',
+            img: './picturs/carrot.jpeg',
+            calories: 100,
+            isReady: false
+        }
+    ])
+
+    function setReadyOnItem(id) {
+        // runnig over the list 
+        // copy item
+        // change isReady if id match
+        // save new item
+        setItems(items.map(item => {
             const newItem = { ...item };
             if (newItem.id === id)
                 item.isReady = !item.isReady;
@@ -44,15 +40,13 @@ export default class MyKitchen extends React.Component {
         }))
     }
 
-    render() {
-        return (
-            <>
-                <div>Recipes made: {this.state.list.filter(item => !item.isReady).length}</div>
-                <Recipies list={this.state.list.filter(item => !item.isReady)} onClick={(id) => { this.setReadyOnItem(id) }}></Recipies>
+    return (
+        <>
+            <div>Recipes made: {items.filter(item => !item.isReady).length}</div>
+            <Recipies list={items.filter(item => !item.isReady)} onClick={(id) => { setReadyOnItem(id) }}></Recipies>
 
-                <div>Recipes made: {this.state.list.filter(item => item.isReady).length}</div>
-                <Recipies list={this.state.list.filter(item => item.isReady)} onClick={(id) => { this.setReadyOnItem(id) }}></Recipies>
-            </>
-        );
-    }
+            <div>Recipes made: {items.filter(item => item.isReady).length}</div>
+            <Recipies list={items.filter(item => item.isReady)} onClick={(id) => { setReadyOnItem(id) }}></Recipies>
+        </>
+    );
 }
